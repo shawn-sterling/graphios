77c35c5deacf32470e3c51b5af967308d9ee2758
Shawn Sterling <shawn@systemtemplar.org>
2014-09-04 01:26:12 -0700

    getting read for modular backend, adding more cfg file options

--- graphios.py
+++ graphios.py
@@ -50,39 +50,50 @@ import time
 
 # nagios spool directory
 spool_directory = '/var/spool/nagios/graphios'
-
-# Where to look for pluggable back-ends
-bdir = '/home/shawn/git/github/graphios.dave/backends'
-
-# graphios log info
-log_file = '/home/shawn/git/github/graphios.dave/graphios.log'
+#
+# # Where to look for pluggable back-ends
+# bdir = '/home/shawn/git/github/graphios.dave/backends'
+#
+# # graphios log info
+log_file = ''
 log_max_size = 25165824         # 24 MB
-# log_level = logging.INFO
-log_level = logging.DEBUG      # DEBUG is quite verbose
-
-# How long to sleep between processing the spool directory
-sleep_time = 15
-
-# when we can't connect to carbon, the sleeptime is doubled until we hit max
-sleep_max = 480
-
-# keep a replayable archive log of processed metrics
-metric_archive = '/usr/local/nagios/var/graphios_metric_archive.log'
-
-# test mode makes it so we print what we would add to carbon, and not delete
-# any files from the spool directory. log_level must be DEBUG as well.
-test_mode = False
-
-# Character to use as replacement for invalid characters in metric names
-replacement_character = '_'
-
-# use service description as part of your carbon metric
-# $GRAPHIOSPREFIX.$HOSTNAME.$SERVICEDESC.$GRAPHIOSPOSTFIX.$PERFDATA
-use_service_desc = False
-
+# # log_level = logging.INFO
+# log_level = logging.DEBUG      # DEBUG is quite verbose
+#
+# # How long to sleep between processing the spool directory
+# sleep_time = 15
+#
+# # when we can't connect to carbon, the sleeptime is doubled until we hit max
+# sleep_max = 480
+#
+# # keep a replayable archive log of processed metrics
+# metric_archive = '/usr/local/nagios/var/graphios_metric_archive.log'
+#
+# # test mode makes it so we print what we would add to carbon, and not delete
+# # any files from the spool directory. log_level must be DEBUG as well.
+# test_mode = False
+#
+# # Character to use as replacement for invalid characters in metric names
+# replacement_character = '_'
+#
+# # use service description as part of your carbon metric
+# # $GRAPHIOSPREFIX.$HOSTNAME.$SERVICEDESC.$GRAPHIOSPOSTFIX.$PERFDATA
+# use_service_desc = False
+#
 # config file stuff
-config_dir = '/home/shawn/git/github/graphios.dave/'
-config_file = 'graphios.cfg'
+
+#-------------------------------------------------
+
+# by default we will check the current path for graphios.cfg, if config_file
+# is set, we will use that instead.
+
+config_file = ''
+debug = True
+
+# config dictionary
+cfg = {}
+
+# default config values (these will be over-ridden with the config
 
 # #### You should stop changing things unless you know what you are doing #####
 ##############################################################################
@@ -100,16 +111,20 @@ parser.add_option("--spool-directory", dest="spool_directory",
 parser.add_option("--log-file", dest="log_file",
                   default=log_file,
                   help="file to log to")
+parser.add_option("--backend", dest="backend", default="stdout",
+                  help="sets which storage backend to use")
+parser.add_option("--config", dest="config_file", default="",
+                  help="set custom config file location")
 
 log = logging.getLogger('log')
 
-# import the backend plug-ins
-bfiles = [fname[:-3] for fname in os.listdir(bdir) if fname.endswith(".py")]
+# # import the backend plug-ins
+# bfiles = [fname[:-3] for fname in os.listdir(bdir) if fname.endswith(".py")]
 
-if bdir not in sys.path:
-    sys.path.insert(1, bdir)
+# if bdir not in sys.path:
+#     sys.path.insert(1, bdir)
 
-backends = [__import__(fname) for fname in bfiles]
+# backends = [__import__(fname) for fname in bfiles]
 
 
 class GraphiosMetric(object):
@@ -138,50 +153,96 @@ class GraphiosMetric(object):
             self.PERFDATA is not '' and
             self.HOSTNAME is not ''
         ):
-            if use_service_desc:
+            if cfg["use_service_desc"]:
                 if self.SERVICEDESC is not '':
                     self.VALID = True
             else:
                 self.VALID = True
 
 
-def read_config(config_dir, config_file):
+def read_config(config_file):
+    if config_file == '':
+        config_file = "%s/graphios.cfg" % sys.path[0]
     config = SafeConfigParser()
-    file_path = os.path.join(config_dir, config_file)
-    log.debug('cfg file name:% s' % os.path.join(config_dir, config_file))
-    if os.path.isfile(file_path):
-        config.read(file_path)
+    # The logger won't be initialized yet, so we use print_debug
+    if os.path.isfile(config_file):
+        config.read(config_file)
         config_dict = {}
         for section in config.sections():
-            log.debug("section: %s" % section)
-            config_dict[section] = {}
-            config_dict[section]['name'] = section
+            # there should only be 1 'graphios' section
+            print_debug("section: %s" % section)
+            config_dict['name'] = section
             for name, value in config.items(section):
-                config_dict[section][name] = value
-                log.debug("config[%s][%s]=%s" % (section, name, value))
+                config_dict[name] = value
+                print_debug("config[%s]=%s" % (name, value))
+        # print config_dict
         return config_dict
     else:
-        log.critical("Can't open filename: %s" % file_path)
+        print_debug("Can't open config file: %s" % config_file)
+        print """\nEither modify the script at the config_file = '' line and
+specify where you want your config file to be, or create a config file
+in the above directory (which should be the same dir the graphios.py is in)
+or you can specify --config=myconfigfilelocation at the command line."""
+        sys.exit(1)
 
 
-def configure(opts):
-    global spool_directory
+def verify_config(config_dict):
+    """
+    will verify the needed variables are found
+    """
+    ensure_list = ['replacement_character', 'log_file', 'log_max_size',
+                   'log_level', 'sleep_time', 'sleep_max', 'test_mode']
+    missing_values = []
+    for ensure in ensure_list:
+        if ensure not in config_dict:
+            missing_values.append(ensure)
+    if len(missing_values) > 0:
+        print "\nMust have value in config file for:\n"
+        for value in missing_values:
+            print "%s\n" % value
+        sys.exit(1)
+
+
+def print_debug(msg):
+    """
+    prints a debug message if global debug is True
+    """
+    if debug:
+        print msg
+
+
+def configure(opts=''):
+    """
+    sets up graphios config
+    """
+    global cfg
+    global debug
+    if opts != '':
+        cfg["log_file"] = opts.log_file
+        cfg["log_max_size"] = 25165824         # 24 MB
+        if opts.verbose:
+            cfg["debug"] = True
+        cfg["spool_directory"] = opts.spool_directory
+        cfg["backend"] = opts.backend
+
+    if cfg["log_file"] == "''":
+        cfg["log_file"] = "%s/graphios.log" % sys.path[0]
 
     log_handler = logging.handlers.RotatingFileHandler(
-        opts.log_file, maxBytes=log_max_size, backupCount=4, encoding='bz2')
+        cfg["log_file"], maxBytes=cfg["log_max_size"], backupCount=4,
+        encoding='bz2')
     formatter = logging.Formatter(
         "%(asctime)s %(filename)s %(levelname)s %(message)s",
         "%B %d %H:%M:%S")
     log_handler.setFormatter(formatter)
     log.addHandler(log_handler)
 
-    if opts.verbose:
+    if "debug" in cfg and cfg["debug"] is True:
         log.setLevel(logging.DEBUG)
         log.addHandler(logging.StreamHandler())
+        debug = True
     else:
-        log.setLevel(log_level)
-
-    spool_directory = opts.spool_directory
+        log.setLevel(logging.INFO)
 
 
 def process_log(file_name):
@@ -198,7 +259,7 @@ def process_log(file_name):
         host_data_file = open(file_name, "r")
         file_array = host_data_file.readlines()
         host_data_file.close()
-    except Exception, ex:
+    except OSError as ex:
         log.critical("Can't open file:%s error: %s" % (file_name, ex))
         sys.exit(2)
 
@@ -212,7 +273,7 @@ def process_log(file_name):
         variables = line.split('\t')
         for var in variables:
             (var_name, value) = var.split('::')
-            value = re.sub("/", replacement_character, value)
+            value = re.sub("/", cfg["replacement_character"], value)
             if re.search("PERFDATA", var_name):
                 mobj.PERFDATA = value
             elif re.search("^\$_", value):
@@ -239,18 +300,13 @@ def handle_file(file_name, graphite_lines):
     """
     archive processed metric lines and delete the input log files
     """
-    if graphite_lines > 0:
-        log.debug("archiving file, %s" % file_name)
-        infile = open(file_name, "r")
-        archive = open(metric_archive, "a")
-        archive.write(infile.read())
-        archive.close()
-        infile.close()
-
-    try:
-        os.remove(file_name)
-    except Exception, ex:
-        log.critical("couldn't remove file %s error:%s" % (file_name, ex))
+    if cfg["test_mode"] and graphite_lines > 0:
+        log.debug("graphite_lines:%s" % graphite_lines)
+    else:
+        try:
+            os.remove(file_name)
+        except OSError as ex:
+            log.critical("couldn't remove file %s error:%s" % (file_name, ex))
 
 
 def process_spool_dir(directory):
@@ -259,7 +315,6 @@ def process_spool_dir(directory):
     """
     log.debug("Processing spool directory %s", directory)
     num_files = 0
-    metric_objects = []
     perfdata_files = os.listdir(directory)
     for perfdata_file in perfdata_files:
         mobjs = []
@@ -275,33 +330,50 @@ def process_spool_dir(directory):
         num_files += 1
         file_dir = os.path.join(directory, perfdata_file)
         mobjs = process_log(file_dir)
-        metric_objects.extend(mobjs)
-        handle_file(file_dir, len(mobjs))
+
+        # we can't remove the file yet, because we don't know if it was sent
+        # to a backend, we need at least one backend to succeed before we
+        # delete it.
+
+        if send_backends(mobjs) > 1:
+            handle_file(file_dir, len(mobjs))
 
     log.info("Processed %s files in %s", num_files, directory)
-    return metric_objects
+    # return metric_objects
+
+
+def send_backends(metrics):
+    """
+    will send to enabled backends, i'm thinking it returns how many backends
+    succeded?
+    """
+    pass
 
 
 def main():
-    while True:
-        metrics = []
-        log.info("graphios startup.")
-        metrics = process_spool_dir(spool_directory)
+    log.info("graphios startup.")
+    try:
+        while True:
+            process_spool_dir(spool_directory)
+            log.debug("graphios sleeping.")
+            time.sleep(cfg["sleep_time"])
+    except KeyboardInterrupt:
+        log.info("ctrl-c pressed. Exiting graphios.")
 
-        if len(metrics) > 0:
-            for backend in backends:
-                bret = backend.send(metrics)
-                if (bret is not True):
-                    log.warn("Plugin returned an error: %s",
-                             backend)
-
-        log.debug("graphios sleeping.")
-        time.sleep(sleep_time)
 
 if __name__ == '__main__':
-    (options, args) = parser.parse_args()
-    configure(options)
-    config_dict = read_config(config_dir, config_file)
-    print config_dict
+    # global cfg
+    if len(sys.argv) > 1:
+        (options, args) = parser.parse_args()
+        if options.config:
+            cfg = read_config(options.config)
+            verify_config(cfg)
+        else:
+            configure(options)
+    else:
+        cfg = read_config(config_file)
+        verify_config(cfg)
+        configure()
+    print cfg
     sys.exit(1)
     main()
