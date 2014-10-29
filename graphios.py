dbcbe7eae344ecbd2aaa1a4ac0db397d473a4096
Bikram Adhikari <salik.adhikari@gmail.com>
2014-10-29 17:31:06 +0545

    If directory in spool folder skip it
    When using PNP4Nagios and using its own spool directory. There are some folder inside which causes error when operating with graphius

--- graphios.py
+++ graphios.py
@@ -354,6 +354,8 @@ def process_spool_dir(directory):
         file_dir = os.path.join(directory, perfdata_file)
         if check_skip_file(perfdata_file, file_dir):
             continue
+        if os.path.isdir(file_dir)
+            continue
         num_files += 1
         mobjs = process_log(file_dir)
         mobjs_len = len(mobjs)
